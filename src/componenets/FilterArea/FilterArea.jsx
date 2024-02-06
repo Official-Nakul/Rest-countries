b7a1266b929870e863e805f26a7002f263a9072a
Official-Nakul <visavadianakulyt@gmail.com>
2024-02-06 20:56:10 +0530

    add filter

--- src/componenets/FilterArea/FilterArea.jsx
+++ src/componenets/FilterArea/FilterArea.jsx
@@ -47,7 +47,7 @@ const FilterArea = ({ isDark, handleSearch, handleRegion }) => {
         >
           <option value="">Filter by Region</option>
           <option value="africa">Africa</option>
-          <option value="america">America</option>
+          <option value="americas">America</option>
           <option value="asia">Asia</option>
           <option value="europe">Europe</option>
           <option value="oceania">Oceania</option>
